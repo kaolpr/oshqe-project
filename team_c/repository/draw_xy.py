e983bca19d54f14e0e03429947156d95bba2a7f7
Tobiasz Fic <tobiasz.fic.stud@pw.edu.pl>
2025-05-27 09:10:47 +0200

    Adding circle generating

--- team_c/repository/draw_xy.py
+++ team_c/repository/draw_xy.py
@@ -1,67 +1,67 @@
+
 from artiq.experiment import *
-# from common import Scope
-import numpy as np
+import numpy
 
 class DrawXY(EnvExperiment):
     def build(self):
         self.setattr_device("core")
-        self.ttl = self.get_device("ttl1") # trigger
-
+        self.ttl = self.get_device("ttl1") # As a trigger
         self.fastino = self.get_device("fastino0")
 
-        self.setattr_argument(
-            f"Scope_horizontal_scale", EnumerationValue(
-                ["1 us", "2 us", "4 us", "10 us", "20 us", "40 us", "100 us", "200 us", "400 us"],
-                default="10 us"
-            )
-        )
-
-        self.setattr_argument(
-            f"Delay_multiplier", NumberValue(
-                default = 8,
-                precision = 0,
-                unit = "",
-                type = "int",
-                step = 1,
-                min = 1,
-                max = 256,
-                scale=1
-            ),
-            tooltip="Samples are send every 392 ns times this multiplier."
-        )
-
-
-    def prepare(self):
-        # Use these parameters
-        self.Amplitude = 2 * V
-        self.sample_num = 200
-
-        t = np.linspace(0,2*np.pi, self.sample_num)
-        self.circle_x = np.sin(t)
-        self.circle_y = np.cos(t)
+        Amplitude = 2 * V
+        sample_num = 48
+        phase= numpy.pi/12
+        self.samples_x=[Amplitude * numpy.sin(2*numpy.pi*i / sample_num * 2 - phase) for i in range(sample_num)]
+        self.samples_y=[Amplitude * numpy.cos(2*numpy.pi*i / sample_num * 2) for i in range(sample_num)]
 
     @kernel
     def run(self):
+        # Reset our system after previous experiment
         self.core.reset()
-        self.core.break_realtime()
-        t0=now_mu()
-        # while True:
-        #     self.ttl.pulse(1*ms)
-        #     delay(1*ms)
-        self.ttl.pulse(50*ns)
-        # at_mu(t0-self.core.seconds_to_mu(1.2*us)) 
-        at_mu(now_mu()-self.core.seconds_to_mu(1.6*us)) 
-        try:
-            for i in range(len(self.circle_x)):
-                sample_x = self.circle_x[i]
-                sample_y = self.circle_y[i]
-                self.fastino.set_dac(dac=0, voltage=sample_x*V)
-                self.fastino.set_dac(dac=1, voltage=sample_y*V)
-                delay(392 * ns)
-                # delay(392 * self.Delay_multiplier * ns)
+        self.fastino.init()
 
-#---------------------------------------------------------------------
+
+        # Set SYSTEM time pointer in future
+        self.core.break_realtime()
+
+        # Trigger for the oscilloscope
+        
+        self.ttl.pulse(50*ns)
+        # Rewind timeline - Fastino takes around 1.2 us to output a sample
+        at_mu(now_mu()-self.core.seconds_to_mu(1.2 * us)) 
+
+        # Calculate and output a sine waveform using numpy.sin
+
+        # Use these parameters
+        self.fastino.set_dac(dac=0, voltage=self.samples_x[0])
+        delay(392*ns * 1)
+        self.fastino.set_dac(dac=1, voltage=self.samples_y[0])
+        delay(392*ns * 1)
+
+        try:
+            sample_num = len(self.samples_x)
+            assert(len(self.samples_x) == len(self.samples_y))
+            for i in range(sample_num):
+                sample_x = self.samples_x[i]
+                sample_y = self.samples_y[i]
+                
+                self.fastino.set_dac(dac=0, voltage=sample_x)
+                delay(392*ns * 1)
+                self.fastino.set_dac(dac=1, voltage=sample_y)
+                delay(392*ns * 1)
+                # Try to change the multiplier; leave 392*ns unchanged
+                # (or don't and see what happens, it may be subtle :) )
+                
+
+# ---------------------------------------------------------------------
 
         except RTIOUnderflow:
+            # Catch RTIO Underflow to leave system in known state
             print("Rtio underflow, cleaning up")
             self.core.break_realtime()
+
+        finally:
+            # Clean up even if RTIO Underflow happens
+            delay(40*us)
+            self.fastino.set_dac(dac=0, voltage=0.0*V)
+
