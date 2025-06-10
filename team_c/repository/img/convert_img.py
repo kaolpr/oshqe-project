fa89c9355c4bff8027927459ac87c83d34aa640e
Tobiasz Fic <tobi.fic@gmail.com>
2025-06-10 09:05:44 +0200

    Add loop finding

--- team_c/repository/img/convert_img.py
+++ team_c/repository/img/convert_img.py
@@ -1,6 +1,8 @@
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.spatial import ConvexHull
+
 
 # Load image
 image = Image.open("img/logo4.png").convert("L")
@@ -21,19 +23,27 @@ x = x_coords / width - 0.5
 y = 0.5 - y_coords / height
 
 # Optional: downsample for speed
-x = x[::1]
-y = y[::1]
+x = x[::8]
+y = y[::8]
+points = np.array([(x_i, y_i) for x_i, y_i in zip(x,y)])
+hull = ConvexHull(points)
+loop_points = points[hull.vertices]
+loop_points = np.append(loop_points, [loop_points[0]], axis=0)
+
+x = [x_i for x_i, _ in loop_points]
+y = [y_i for _, y_i in loop_points]
+
 print(f"Number of points: {len(x)}")
 
-# save to file "converted_arrays.csv"
-# give title "x,y"
+
+
 
 np.savetxt("converted_arrays.csv", np.column_stack((x, y)), delimiter=",", header="x,y", comments='')
 
-# # Plot
-# plt.figure(figsize=(6, 6))
-# plt.scatter(x, y, s=0.2, color="black")
-# plt.axis("equal")
-# plt.axis("off")
-# plt.tight_layout()
-# plt.show()
+# Plot
+plt.figure(figsize=(6, 6))
+plt.plot(x, y, color="black")
+plt.axis("equal")
+plt.axis("off")
+plt.tight_layout()
+plt.show()
